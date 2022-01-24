eef0c81f3cb5405da1b03e6e12428470435461d8
P. HYAREEN LINDA <72100336+Hyareen-Linda@users.noreply.github.com>
2022-01-25 00:29:56 +0530

    Update blog-uc.js

--- js/blog-uc.js
+++ js/blog-uc.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 25, 2022 00:00:00").getTime();
+var countDownDate = new Date("Jan 25, 2022 23:59:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
